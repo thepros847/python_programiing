b8dde38adec832f0781d3b4665aa7d04c494db1c
thepros847 <88582086+thepros847@users.noreply.github.com>
2021-08-14 11:43:21 +0000

    Add input_processing_output.py

---
+++ input_processing_output.py
@@ -0,0 +1,3 @@
+x = float(input("Enter a number: "))
+y = 2 * x + 4
+print("The output of y is: ",y)
---
+++ input_processing_output_2.py
@@ -0,0 +1,3 @@
+y = float(input("Enter a number: "))
+x = 16 * y + 8
+print("The output of x is: ",x)
