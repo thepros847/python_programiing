e2512442bb0f5f0c39c653886b39407c1febaa10
thepros847 <88582086+thepros847@users.noreply.github.com>
2021-08-21 12:49:36 +0000

    operator number

---
+++ lessons/operators.py
@@ -0,0 +1,72 @@
+# create a variable x and assign a value 10
+x = 10
+
+# create a variable y and assign a value 25
+y = 25
+
+print(x+y)
+
+print(x-y)
+
+print(x/y)
+
+print(x*y)
+
+print(x**y)
+
+# floor division
+#create a variable  and assign a value 12.68
+a = 12.6
+
+#create a variable and assign b value 3
+b = 3
+
+print(a//b) 
+
+print(a%b)
+
+print(-x)
+
+print(abs(-x))
+
+
+
+
+string_1="Python"
+
+string_2="Programming"
+
+final_string=string_1 + string_2
+
+print(final_string)
+
+
+
+print(string_1*4)
+
+
+print(x==y)
+
+print(x is 10)
+
+print(x == 10)
+
+print(x != y)
+
+print(x <= y)
+
+print(x >= 20)
+
+print(x < y)
+
+#print(a & b)
+
+#print(a | b)
+
+#print(a ^ b)
+
+#print(a << x)
+
+#print(a >> x)
+
+#print(~a)
