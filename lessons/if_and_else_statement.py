cc5a9e8fc3bdbd8971fe26f78b26f2761dfd2913
thepros847 <thepros847@gmail.com>
2021-08-28 12:11:27 +0000

    if_and_else_statement.py

---
+++ lessons/if_and_else_statement.py
@@ -0,0 +1,6 @@
+ineger_number = int(input("Enter an integer number:"))
+
+if ineger_number%2 == 0:
+	print(ineger_number, "is an even number!")
+else:
+	print(ineger_number, "is an odd number!")
---
+++ lessons/or_statement.py
@@ -0,0 +1,23 @@
+won_bet = True
+big_win = True 
+
+if  won_bet or big_win:
+	print("You can now stop betting!")
+
+	won_bet = False
+big_win = True 
+
+if  won_bet or big_win:
+	print("You can now stop betting!")
+
+	won_bet = True
+big_win = False
+
+if  won_bet or big_win:
+	print("You can now stop betting!")
+
+	won_bet = False
+big_win = False
+
+if  won_bet or big_win:
+	print("You can now stop betting!")
