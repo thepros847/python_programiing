1def0ed4701ed7714294e53d881933ae3bce1a77
thepros847 <thepros847@gmail.com>
2021-09-04 10:35:57 +0000

    Create student_score.py

---
+++ assingment/student_score.py
@@ -0,0 +1,9 @@
+student_score = float(input("Enter student exams score:"))
+
+if student_score == 90:
+	print("student gets a laptop")
+elif student_score == 60:
+	print("student gets a tablet")
+else student_score < 60:
+	print (" student gets nothing")
+	
