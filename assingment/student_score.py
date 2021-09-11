bf9d3c7c4b3192e650db7c716adb7a1b5abc7ade
thepros847 <thepros847@gmail.com>
2021-09-11 10:46:09 +0000

    lessons

--- assingment/student_score.py
+++ assingment/student_score.py
@@ -4,5 +4,7 @@ if student_score >= 90:
     print("student gets a laptop")
 elif student_score >= 59:
     print("student gets a tablet")
+elif student_score >= 0:
+    print("student gets a nothing")
 else: 
-    print (" student gets nothing")
+    print (" student not specify")
--- lessons/prime_number_multiple_statement.py
+++ lessons/prime_number_multiple_statement.py
@@ -1,1 +1,14 @@
+while Student_score = float(input("Enter student exams score:")):
+if student_score >= 90:
+    print("student gets a laptop")
+elif Student_score >= 59:
+    print("student gets a tablet")
+else: 
+    print (" student gets nothing")	
+
+     break
+    print ("student score is invalid enter new score")
+
+
+
 
